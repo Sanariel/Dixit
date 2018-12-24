6b58289bc09e59ed8822f5d769759ae8f85f232b
Sanariel <46084511+Sanariel@users.noreply.github.com>
2018-12-24 11:15:45 +0100

    Create dixit.js

---
+++ .gitignore/commands/dixit.js
@@ -0,0 +1,15 @@
+const Command = require('./command.js')
+
+module.exports = class Dixit extends Command {
+
+    static match(message) {
+        return message.content.match("di")
+    }
+    static action(message) {
+        let args = message.content.split('di')
+        args.shift()
+
+        message.reply(args.join(""))
+    }
+
+}
