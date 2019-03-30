6f8c5e6555310cf27400ce934f3398a8f1961ca4
Sanariel <46084511+Sanariel@users.noreply.github.com>
2019-03-30 15:25:15 +0100

    Update dixit.js

--- .gitignore/commands/dixit.js
+++ .gitignore/commands/dixit.js
@@ -3,7 +3,7 @@ const Command = require('./command.js')
 module.exports = class Dixit extends Command {
 
     static match(message) {
-        return message.content.match(/di?/gi)
+        return message.content.match(/di/gmi)
     }
     static action(message) {
         let args = message.content.split(/di?/gi)
